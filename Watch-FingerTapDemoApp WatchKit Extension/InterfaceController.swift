c0229c9466f04b992c2eeb658680a0e2a4cf2eab
ElijahButers <Elijahbuters@gmail.com>
2015-12-05 16:38:24 +0200

    Added code to Actions, func start(), func stop(), func result().

--- Watch-FingerTapDemoApp WatchKit Extension/InterfaceController.swift
+++ Watch-FingerTapDemoApp WatchKit Extension/InterfaceController.swift
@@ -13,17 +13,20 @@ import Foundation
 class InterfaceController: WKInterfaceController {
     
     // Outlets
-    @IBOutlet var leftTimeLabel: WKInterfaceLabel!
+    @IBOutlet var timeLeftLabel: WKInterfaceLabel!
     @IBOutlet var buttonTap: WKInterfaceButton!
     @IBOutlet var startButton: WKInterfaceButton!
     @IBOutlet var bestScoreLabel: WKInterfaceLabel!
     
-    
+    var timer = NSTimer()
+    var count = 10
+    var tapCount = 0
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        self.buttonTap.setEnabled(false)
     }
 
     override func willActivate() {
@@ -37,14 +40,65 @@ class InterfaceController: WKInterfaceController {
     }
     
     // Actions
-    
     @IBAction func buttonTapped() {
+        tapCount += 1
+        self.buttonTap.setTitle("\(tapCount)")
     }
     
     @IBAction func startTapped() {
+        start()
+        self.buttonTap.setEnabled(true)
+        self.startButton.setEnabled(false)
     }
     
     @IBAction func resetTapped() {
+        stop()
+        count = 10
+        tapCount = 0
+        self.timeLeftLabel.setText("\(count) sec")
+        self.buttonTap.setTitle("Tap!")
+        self.buttonTap.setEnabled(false)
+        self.startButton.setEnabled(true)
+    }
+    
+    func start() {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+    }
+    
+    func stop() {
+        
+        timer.invalidate()
+    }
+    
+    func result() {
+        
+        if count == 0 {
+            self.buttonTap.setEnabled(false)
+            self.timeLeftLabel.setText("\(count) sec")
+        } else {
+            count--
+            self.timeLeftLabel.setText("\(count) sec")
+        }
+        
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
